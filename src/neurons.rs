fcb65f1fe65b84307ee91ab8f3347d6ba90c6618
Another-Ashl3y <another.ashl3y@gmail.com>
2024-07-26 23:24:03 +0100

    readme update

--- src/neurons.rs
+++ src/neurons.rs
@@ -3,22 +3,29 @@ enum Activation {
     Step,
     Relu
 }
+
+
 enum Node_Type {
     Normal(Neuron),
     Memory(Memory)
 }
+
+
 pub struct Neuron {
     activation: Activation,
     indices: Vec<usize>,
     weights: Vec<f64>,
     bias: f64
 }
+
+
 pub struct Memory {
-    activation: Activation,
-    indices: Vec<usize>,
-    weights: Vec<f64>,
-    bias: f64
+    write: usize,
+    read: usize,
+    data: usize
 }
+
+
 pub struct Network {
     neurons: Vec<Node_Type>
 }
